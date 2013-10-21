10b93e413e7cdf082492f5c42de1630b7cc966f0
LesyaBobruyko <olesya.bobruyko@gmail.com>
2013-10-21 15:19:47 +0300

    add current week birthday 21/10/2013

--- public/js/views/Birthdays/ContentView.js
+++ public/js/views/Birthdays/ContentView.js
@@ -18,23 +18,43 @@ function (ListTemplate, EmployeesCollection, ListItemView, common, Custom) {
         render: function () {
             Custom.setCurrentCL(this.employeesCollection.models.length);
             console.log('Render Birthday View');
-            var models = [],
-                birthday;
+            var now = new Date();
+            var monthModels = [],
+                weekModels = [],
+                birthday,
+                today = new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+                lastDay = new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+                daysToAdd = 7 - today.getDay();
             this.$el.html(_.template(ListTemplate));
             var list = this.$el.find('#birthdaysList');
-
+            lastDay.setDate(today.getDate() + daysToAdd);
             _.each(this.employeesCollection.models, function (model) {
                 if (model.get('dateBirth')) {
-                    birthday = new Date(common.ISODateToDate(model.get('dateBirth')).replace(/-/g, '/'));
-                    if (birthday.getMonth() == new Date().getMonth() && birthday.getDate()>=new Date().getDate()) {
-                        models.push(model);
+                    birthday = new Date(common.ISODateToDate(model.get('dateBirth')));
+                    if (birthday.getMonth() == today.getMonth() && birthday.getDate() >= today.getDate()) {
+                        monthModels.push(model);
                     }
-                }
+                    birthday.setFullYear(today.getFullYear());
+                    birthday.setHours(0);
+                    var valueOfBirthday = birthday.valueOf();
+                    if ((valueOfBirthday <= lastDay.valueOf() && valueOfBirthday >= today.valueOf())) {
+                            weekModels.push(model);
+                        }
+                    }
             }, this);
-            _.each(models, function (model) {
-                list.append(new ListItemView({ model: model }).render().el);
+            _.each(weekModels, function (model) {
+                list.find("#weekList").append(new ListItemView({ model: model }).render().el);
             }, this);
-
+            _.each(weekModels, function (weekModel) {
+                _.each(monthModels, function (model) {
+                    if (weekModel.get('_id') == model.get('_id')) {
+                        monthModels.splice(monthModels.indexOf(model), 1);
+                    }
+                }, this);
+            }, this);
+            _.each(monthModels, function (model) {
+                list.find("#monthList").append(new ListItemView({ model: model }).render().el);
+            }, this);
         }
     });
 
