5da25084cd30402a5828632071885db2899f74b1
Антон <antonsav89@gmail.com>
2013-11-15 18:07:24 +0200

    google calendar sync start develop 15.11.2013

--- public/js/GoogleAuth.js
+++ public/js/GoogleAuth.js
@@ -1,18 +1,20 @@
 define(
     function () {
+        var Authorized = false;
         var authUrl    =   'https://accounts.google.com/o/oauth2/auth?';
         var validURI    =   'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=';
         var clientId    =   '38156718110.apps.googleusercontent.com';
         var redirectURI    =   'http://localhost:8088';
         var respType        =   'token';
-        var scope       = "openid%20email";
-        var userProfileScope = "https://www.googleapis.com/plus/v1/people/me";
-        var _url        =   authUrl + 'scope=' + scope + '&client_id=' + clientId + '&redirect_uri=' + redirectURI + '&response_type=' + respType;
+        var scope       = "openid email https://www.google.com/calendar/feeds/";
+        var userProfileScope = "https://www.googleapis.com/plus/v1/people/me?";
+        var calendarList = "https://www.googleapis.com/calendar/v3/users/me/calendarList?";
+        var getCalendar = "https://www.googleapis.com/auth/calendar.readonly";
+        var _url        =   authUrl + 'scope=' + scope + '&client_id=' + clientId + '&redirect_uri=' + redirectURI + '&response_type=' + respType + '&approval_prompt=force';
         var winObject;
+        var acToken;
 
-
-        var authorize = function(){
-
+        var authorize = function(callback){
             var strWindowFeatures = "resizable=yes,width=800, height=600";
             var winObject = window.open(_url, "Google Authorization", strWindowFeatures);
             var pollTimer = window.setInterval(function(){
@@ -20,13 +22,33 @@ define(
                     window.clearInterval(pollTimer);
                     var url = winObject.document.URL;
                     console.log(url);
-                    var acToken = parseURL(url, 'access_token');
+                    acToken = parseURL(url, 'access_token');
                     var tokenType = parseURL(url, 'token_type');
                     var expiresIn = parseURL(url, 'expires_in');
+                    this.Authorized = true;
                     winObject.close();
-                    validateToken(acToken);
+                    validateToken(acToken, function(resp){
+                        callback(resp);
+                    });
                 }
             }, 100);
+        }
+
+        var getCalendarEvents = function(){
+            $.ajax({
+                type: "GET",
+                url: userProfileScope + "access_token=" + acToken,
+                data: null,
+                success: function(response){
+                    if(response.error){
+                        throw new Error(response.error.message);
+                    }
+                },
+                error: function (error){
+                    throw new Error(error.message);
+                },
+                dataType: "jsonp"
+            });
         }
 
         var parseURL = function(url, name){
@@ -40,21 +62,24 @@ define(
                 return results[1];
         }
 
-        var validateToken = function(token){
+        var validateToken = function(token, callback){
             $.ajax({
                 type:"GET",
                 url: validURI + token,
                 data: null,
                 success: function(response){
                     console.log(response);
+                    //getCalendarsList();
+                    callback(response);
                 },
                 dataType: "jsonp"
             });
         }
 
 
-
         return {
-            Authorize: authorize
+            Authorize: authorize,
+            Authorized: Authorized,
+            getCalendarEvents:getCalendarEvents
         }
     });
