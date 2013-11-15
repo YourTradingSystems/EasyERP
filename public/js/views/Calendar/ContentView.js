7bf74e4b1bba6b5573a099fde8072b2243ccb17a
Антон <antonsav89@gmail.com>
2013-11-15 10:15:24 +0200

    google calendar sync start develop 13.11.2013

--- public/js/views/Calendar/ContentView.js
+++ public/js/views/Calendar/ContentView.js
@@ -2,21 +2,28 @@ define([
     "text!templates/Calendar/CalendarTemplate.html",
     "Calendar",
     "collections/Events/EventsCollection",
-    "common"
+    "common" ,
+    "GoogleAuth"
 ],
-function (CalendarTemplate, Calendar, EventsCollection, common) {
+function (CalendarTemplate, Calendar, EventsCollection, common, GoogleAuth) {
     var ContentView = Backbone.View.extend({
         el: '#content-holder',
         template: _.template(CalendarTemplate),
         initialize: function () {
             this.eventsCollection = new EventsCollection();
             this.eventsCollection.bind('reset', _.bind(this.render, this));
-            this.render();
+            //this.render();
         },
 
-        events: { },
+        events: {
+            "click #authBtn" : "authorize"
+        },
+        authorize: function(){
+            GoogleAuth.Authorize();
+        },
 
         render: function () {
+            console.log('Render Calendar');
             this.$el.html(this.template());
             Calendar.initCalendar("schedulerDiv", this.eventsCollection);
             Calendar.initMiniCalendar("miniCalendar");
