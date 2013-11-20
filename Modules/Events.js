dc87265cbb716fa6adc3d35aebe96c3182d35060
AndrewYanuta <andrew.yanuta@gmail.com>
2013-11-20 15:53:47 +0200

    Calendar&Events ServerSide

--- Modules/Events.js
+++ Modules/Events.js
@@ -1,21 +1,26 @@
 ﻿// JavaScript source code
 var Events = function (logWriter, mongoose) {
 
+    var ObjectId = mongoose.Schema.Types.ObjectId;
+
     var eventsSchema = mongoose.Schema({
-        id: String,
+        _id: Number,
+        id: Number,
+        calendar: [{ type: ObjectId, ref: 'Calendars' }],
         summary: { type: String, default: '' },
-        description: {type: String, default: ''},
+        description: { type: String, default: '' },
         eventType: { type: String, default: '' },
         start_date: Date,
         end_date: Date,
-        color: {type:String, default:''},
-        textColor: {type:String, default:''},
-        assignTo: { type:String, default:''},
+        color: { type: String, default: '' },
+        textColor: { type: String, default: '' },
+        assignTo: { type: String, default: '' },
         status: { type: String, default: '' },
         priority: { type: String, default: '' }
     }, { collection: 'Events' });
 
     var calendarsSchema = mongoose.Schema({
+        _id: String,
         kind: String,
         id: String,
         etag: String,
@@ -38,42 +43,48 @@ var Events = function (logWriter, mongoose) {
                 savetoDb(data);
             }
             function savetoDb(data) {
+                console.log('saving data');
+                console.log(data);
                 try {
                     console.log(data);
                     _event = new event();
-                    _event._id = data.id;
+                    // _event._id = data.id;
+                    if (data.id) {
+                        _event._id = data.id;
+                    }
                     if (data.summary) {
                         _event.summary = data.summary;
-                    }
-                    if (data.priority) {
-                        _event.priority = data.priority;
-                    }
-                    if (data.assignTo) {
-                        _event.assignTo = data.assignTo;
                     }
                     if (data.description) {
                         _event.description = data.description;
                     }
-                   if (data.start_date) {
-                       _event.start_date = data.start_date;
+                    if (data.eventType) {
+                        _event.eventType = data.eventType;
                     }
-                   if (data.end_date) {
-                       _event.end_date= data.end_date;
-                   }
-                   if (data.status) {
-                       _event.status = data.status;
-                   }
-                   if (data.eventType) {
-                       _event.eventType = data.eventType;
-                   }
-                   if (data.color) {
-                       _event.color = data.color;
-                   }
-                   if (data.textColor) {
-                       _event.textColor = data.textColor;
-                   }
+                    if (data.start_date) {
+                        _event.start_date = data.start_date;
+                    }
+                    if (data.end_date) {
+                        _event.end_date = data.end_date;
+                    }
+                    if (data.color) {
+                        _event.color = data.color;
+                    }
+                    if (data.textColor) {
+                        _event.textColor = data.textColor;
+                    }
+                    if (data.assignTo) {
+                        _event.assignTo = data.assignTo;
+                    }
+                    if (data.status) {
+                        _event.status = data.status;
+                    }
+
+                    if (data.priority) {
+                        _event.priority = data.priority;
+                    }
                     ///////////////////////////////////////////////////
-                    _event.save(function(err, result) {
+                    _event.save(function (err, result) {
                         try {
                             if (err) {
                                 console.log(err);
@@ -82,22 +93,93 @@ var Events = function (logWriter, mongoose) {
                             } else {
                                 res.send(201, { success: 'A new event was created successfully' });
                             }
-                        } catch(error) {
+                        } catch (error) {
                             logWriter.log("Events.js create savetoBd _event.save " + error);
                         }
                     });
-                } catch(error) {
+                } catch (error) {
                     console.log(error);
                     logWriter.log("Events.js create savetoBd " + error);
                     res.send(500, { error: 'Events.save  error' });
                 }
             }
-        } catch(exception) {
+        } catch (exception) {
             console.log(exception);
             logWriter.log("Events.js  " + exception);
             res.send(500, { error: 'Events.save  error' });
         }
     };//End create
+
+    function get(response) {
+        var res = {}
+        var description = "";
+        res['data'] = [];
+        var query = event.find();
+        query.sort({ summary: 1 });
+        query.exec(function (err, result) {
+            if (err) {
+                console.log(err);
+                logWriter.log('Events.js get Events.find' + description);
+                response.send(500, { error: "Can't find Events" });
+            } else {
+                res['data'] = result;
+                response.send(res);
+            }
+        });
+    }; //end get
+
+    function update(_id, data, res) {
+        try {
+            console.log(_id);
+            delete data._id;
+            event.findById(_id, function (err, result) {
+                if (err) {
+                    console.log(err);
+                    logWriter.log('Events.js get event.findById' + description);
+                    response.send(500, { error: "Can't find event" });
+                }
+                else if (result) {
+
+                    event.update({ _id: _id }, data, function (err, result) {
+                        try {
+                            if (err) {
+                                console.log(err);
+                                logWriter.log("Events.js update events.update " + err);
+                                res.send(500, { error: "Can't update Events" });
+                            } else {
+                                res.send(200, { success: 'Events updated success' });
+                            }
+                        }
+                        catch (exception) {
+                            logWriter.log("Events.js getEvents event.find " + exception);
+                        }
+                    });
+                } else if (!result) {
+                    data.id = _id;
+                    console.log(data);
+                    create(data, res);
+                }
+            });
+
+        }
+        catch (exception) {
+            console.log(exception);
+            logWriter.log("Events.js update " + exception);
+            res.send(500, { error: 'Events updated error' });
+        }
+    };// end update
+
+    function remove(_id, res) {
+        event.remove({ _id: _id }, function (err, result) {
+            if (err) {
+                console.log(err);
+                logWriter.log("Events.js remove event.remove " + err);
+                res.send(500, { error: "Can't remove Events" });
+            } else {
+                res.send(200, { success: 'Events removed' });
+            }
+        });
+    };// end remove
 
     function createCalendar(data, res) {
         try {
@@ -119,7 +201,7 @@ var Events = function (logWriter, mongoose) {
                     if (data.description) {
                         _calendar.description = data.description;
                     }
-                  
+
                     if (data.kind) {
                         _calendar.kind = data.kind;
                     }
@@ -223,73 +305,7 @@ var Events = function (logWriter, mongoose) {
                 res.send(200, { success: 'Calendar removed' });
             }
         });
-    };// end removeCalendar
-
-    function get(response) {
-        var res = {}
-        var description = "";
-        res['data'] = [];
-        var query = event.find();
-        query.sort({ summary: 1 });
-        query.exec(function (err, result) {
-            if (err) {
-                console.log(err);
-                logWriter.log('Events.js get Events.find' + description);
-                response.send(500, { error: "Can't find Events" });
-            } else {
-                res['data'] = result;
-                response.send(res);
-            }
-        });
-    }; //end get
-
-    function update(_id, data, res) {
-        try {
-            delete data._id;
-            event.findById(_id , function (err, result) {
-                if (err) {
-
-                }
-                else if (result) {
-                    event.update({ _id: _id }, data, function (err, result) {
-                        try {
-                            if (err) {
-                                console.log(err);
-                                logWriter.log("Events.js update events.update " + err);
-                                res.send(500, { error: "Can't update Events" });
-                            } else {
-                                res.send(200, { success: 'Events updated success' });
-                            }
-                        }
-                        catch (exception) {
-                            logWriter.log("Events.js getEvents event.find " + exception);
-                        }
-                    });
-                } else if (!result) {
-                    data.id = _id;
-                    create(data, res);
-                }
-            });
-          
-        }
-        catch (exception) {
-            console.log(exception);
-            logWriter.log("Events.js update " + exception);
-            res.send(500, { error: 'Events updated error' });
-        }
-    };// end update
-
-    function remove(_id, res) {
-        event.remove({ _id: _id }, function (err, result) {
-            if (err) {
-                console.log(err);
-                logWriter.log("Events.js remove event.remove " + err);
-                res.send(500, { error: "Can't remove Events" });
-            } else {
-                res.send(200, { success: 'Events removed' });
-            }
-        });
-    };// end remove
+    };// end removeCalendar  
 
     return {
         create: create,
