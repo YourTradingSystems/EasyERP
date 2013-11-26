f5879b32319aa74a55bf8a74c3ddf8dde94b422b
romashka50 <roma.buchuk@gmail.com>
2013-11-26 13:22:36 +0200

    depServer

--- Modules/Department.js
+++ Modules/Department.js
@@ -6,7 +6,7 @@ var Department = function (logWriter, mongoose, employeeModel, event) {
             id: { type: String, default: '' },
             name: { type: String, default: '' }
         },
-        departmentManager: { type: ObjectId, ref: 'Employees',defaults:null },
+        departmentManager: { type: ObjectId, ref: 'Employees', default:null },
     }, { collection: 'Department' });
 
     var department = mongoose.model('Department', DepartmentSchema);
