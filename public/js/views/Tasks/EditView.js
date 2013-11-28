88692b1aab34880c30b6d3ae583d7fc0ec990ec7
Антон <antonsav89@gmail.com>
2013-11-28 18:09:42 +0200

    Employees & Leads 26.11.2013

--- public/js/views/Tasks/EditView.js
+++ public/js/views/Tasks/EditView.js
@@ -15,7 +15,7 @@
             contentType: "Tasks",
             template: _.template(EditTemplate),
             initialize: function (options) {
-                _.bindAll(this, "render");
+                _.bindAll(this, "render", "saveItem");
                 this.tasksCollection = options.collection;
                 this.currentModel = this.tasksCollection.getElement();
                 this.render();
@@ -24,8 +24,6 @@
             events: {
                 "click #tabList a": "switchTab",
                 "click .breadcrumb a, #Cancel span, #Done span": "changeWorkflow",
-                "click #saveBtn": "saveItem",
-                "click #cancelBtn": "hideDialog",
                 "change #workflowDd": "changeWorkflowValues",
                 'keydown': 'keydownHandler'
             },
@@ -101,12 +99,11 @@
                 var self = this;
 
                 var mid = 39;
-                debugger;
 
-                var project = this.$el.find("#projectDd option:selected").val();
-                var assignedTo = this.$el.find("#assignedToDd option:selected").val();
+                var project = $("#projectDd option:selected").val();
+                var assignedTo = $("#assignedToDd option:selected").val();
 
-                var tags = $.trim(this.$el.find("#tags").val()).split(',');
+                var tags = $.trim($("#tags").val()).split(',');
                 if (tags.length == 0) {
                     tags = null;
                 }
@@ -116,8 +113,8 @@
                     sequence = null;
                 }
 
-                var customer = this.$el.find("#customerDd option:selected").val();
-                var workflow = this.$el.find("#workflowValue option:selected").val();
+                var customer = $("#customerDd option:selected").val();
+                var workflow = $("#workflowValue option:selected").val();
 
 
                 var estimated = $("#estimated").val();
@@ -130,23 +127,23 @@
                     logged = 0;
                 }
 
-                var priority = this.$el.find("#priority option:selected").val();
+                var priority = $("#priority option:selected").val();
 
                 var data = {
-                    type: this.$el.find("#type option:selected").text(),
-                    summary: this.$el.find("#summary").val(),
-                    assignedTo: assignedTo,
-                    workflow: workflow,
-                    project: project,
+                    type: $("#type option:selected").text(),
+                    summary: $("#summary").val(),
+                    assignedTo: assignedTo ? assignedTo : null,
+                    workflow: workflow ? workflow : null,
+                    project: project ? project : null,
                     tags: tags,
-                    deadline: $.trim(this.$el.find("#deadline").val()),
-                    description: this.$el.find("#description").val(),
+                    deadline: $.trim($("#deadline").val()),
+                    description: $("#description").val(),
                     extrainfo: {
                        // priority: priority,
                         sequence: sequence,
                        // customer: customer,
-                        StartDate: $.trim(this.$el.find("#StartDate").val()),
-                        EndDate: $.trim(this.$el.find("#EndDate").val())
+                        StartDate: $.trim($("#StartDate").val()),
+                        EndDate: $.trim($("#EndDate").val())
                     },
                     estimated: estimated,
                     logged: logged
@@ -187,86 +184,36 @@
                 },this);
             },
 
-            populateDropDown: function (type, selectId, url, val) {
-                var selectList = $(selectId);
-                var self = this;
-                var workflowNames = [];
-                this.workflows = [];
-                var my_wId;
-                selectList.append($("<option/>").val('').text('Select...'));
-                dataService.getData(url, { mid: 39 }, function (response) {
-                    var options = $.map(response.data, function (item) {
-                        switch (type) {
-                            case "project":
-                                return self.projectOption(item);
-                            case "person":
-                                return self.personOption(item);
-                            case "priority":
-                                return self.priorityOption(item);
-                            case "workflow": {
-                                 self.workflows.push(item);
-                                 if ((_.indexOf(workflowNames, item.wName)=== -1) ) {
-                                     $(App.ID.workflowDd).append(self.workflowValueOption(item));
-                                 }
-                                 workflowNames.push(item.wName);
-                            }
-                        }
-                    });
-                       selectList.append(options);
-
-                    if(typeof val!="undefined")
-                            selectList.val(val).trigger("change");
-                });
-            },
-            workflowValueOption: function (item) {
-                return (this.currentModel.get("workflow") && this.currentModel.get("workflow").wId === item.wId) ?
-                    $('<option/>').val(item.wId).text(item.wName).attr('selected', 'selected') :
-                    $('<option/>').val(item.wId).text(item.wName);
-            },
-
-            workflowOption: function (item) {
-                return (this.currentModel.get("workflow") && this.currentModel.get("workflow")._id === item._id) ?
-                      $('<option/>').val(item._id).text(item.name).attr('selected', 'selected') :
-                      $('<option/>').val(item._id).text(item.name);
-            },
-            projectOption: function (item) {
-                return this.currentModel.get("project")._id === item._id ?
-                    $('<option/>').val(item._id).text(item.projectName).attr('selected', 'selected') :
-                    $('<option/>').val(item._id).text(item.projectName);
-            },
-            personOption: function (item) {
-                return (this.currentModel.get("assignedTo") && this.currentModel.get("assignedTo")._id === item._id) ?
-                    $('<option/>').val(item._id).text(item.name.first + " " + item.name.last).attr('selected', 'selected') :
-                    $('<option/>').val(item._id).text(item.name.first + " " + item.name.last);
-            },
-            priorityOption: function (item) {
-                return this.currentModel.id === item._id ?
-                    $('<option/>').val(item._id).text(item.priority).attr('selected', 'selected') :
-                    $('<option/>').val(item._id).text(item.priority);
-            },
-
             render: function () {
                 var formString = this.template({
                     model: this.currentModel.toJSON()
                 });
-
+                var self = this;
                 this.$el = $(formString).dialog({
                     dialogClass: "edit-task-dialog",
                     width: "50%",
                     height: 513,
-                    title: this.currentModel.toJSON().project.projectShortDesc
+                    title: this.currentModel.toJSON().project.projectShortDesc,
+                    buttons:{
+                        save:{
+                            text: "Save",
+                            class: "btn",
+                            click: self.saveItem
+                        },
+                        cancel:{
+                            text: "Cancel",
+                            class: "btn",
+                            click: self.hideDialog
+                        }
+                    }
                 });
-                var that = this;
-                this.populateDropDown("project", App.ID.projectDd, "/getProjectsForDd");
-                this.populateDropDown("person", App.ID.assignedToDd, "/getPersonsForDd");
-                this.populateDropDown("priority", App.ID.priorityDd, "/Priority");
-                this.populateDropDown("workflow", App.ID.workflowDd, "/taskWorkflows", that.currentModel.toJSON().workflow._id);
-
-
 
                 $('#StartDate').datepicker({ dateFormat: "d M, yy" });
                 $('#EndDate').datepicker({ dateFormat: "d M, yy" });
                 $('#deadline').datepicker({ dateFormat: "d M, yy" });
+                common.populateProjectsDd(App.ID.projectDd, "/getProjectsForDd", this.currentModel.toJSON());
+                common.populateWorkflows("Task", App.ID.workflowDd, App.ID.workflowNamesDd, "/Workflows", this.currentModel.toJSON());
+                common.populateEmployeesDd(App.ID.assignedToDd, "/getPersonsForDd", this.currentModel.toJSON());
                 this.delegateEvents(this.events);
 
                 return this;
