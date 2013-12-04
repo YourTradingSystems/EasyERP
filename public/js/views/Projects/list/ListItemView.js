a64ba7839dba1a7ef3277b7438a0bd0bfd429c07
Ivan Kornyk <ownmass@gmail.com>
2013-12-04 14:45:23 +0200

    projects form

---
+++ public/js/views/Projects/list/ListItemView.js
@@ -0,0 +1,19 @@
+﻿define([
+    'text!templates/Projects/list/ListTemplate.html'
+],
+
+function (ProjectsListTemplate) {
+    var ProjectsListItemView = Backbone.View.extend({
+        el: '#listTable',
+
+        initialize: function(options) {
+            this.collection = options.collection;
+            this.startNumber = options.startNumber;
+        },
+        render: function() {
+            this.$el.append(_.template(ProjectsListTemplate, { projectsCollection: this.collection.toJSON(), startNumber: this.startNumber }));
+        }
+    });
+
+    return ProjectsListItemView;
+});
