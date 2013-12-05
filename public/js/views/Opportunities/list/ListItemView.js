22b5f99f29b64c916bc0c4d1aa3d12adb7dbe84b
vasya <vasya@vasya-burdun>
2013-12-05 16:56:28 +0200

    Opportunities  -> kanban

---
+++ public/js/views/Opportunities/list/ListItemView.js
@@ -0,0 +1,19 @@
+﻿define([
+    'text!templates/Opportunities/list/ListTemplate.html'
+],
+
+function (OpportunitiesListTemplate) {
+    var OpportunitiesListItemView = Backbone.View.extend({
+        el: '#listTable',
+
+        initialize: function(options) {
+            this.collection = options.collection;
+            this.startNumber = options.startNumber;
+        },
+        render: function() {
+            this.$el.append(_.template(OpportunitiesListTemplate, { opportunitiesCollection: this.collection.toJSON(), startNumber: this.startNumber }));
+        },
+    });
+
+    return OpportunitiesListItemView;
+});
