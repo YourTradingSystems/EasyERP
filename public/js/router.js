b9999eda055d20b340f994aa9aa08d8293c36286
Ivan Kornyk <ownmass@gmail.com>
2013-12-17 18:05:56 +0200

    show more thumbnails persons

--- public/js/router.js
+++ public/js/router.js
@@ -163,7 +163,7 @@ define([
             self = this;
             Custom.setCurrentVT('kanban');
             require([ContentViewUrl, TopBarViewUrl, CollectionUrl], function (ContentView, TopBarView, ContentCollection) {
-                var  collection = new ContentCollection({ viewType: 'kanban', page: 1, count: 5, parrentContentId: parrentContentId });
+                var  collection = new ContentCollection({ viewType: 'kanban', page: 1, count: 10, parrentContentId: parrentContentId });
                 collection.bind('reset', _.bind(createViews, self));
                 function createViews() {
                     collection.unbind('reset');
@@ -202,7 +202,7 @@ define([
             self = this;
             Custom.setCurrentVT('thumbnails');
             require([ContentViewUrl, TopBarViewUrl, CollectionUrl], function (ContentView, TopBarView, ContentCollection) {
-                collection = (contentType !== 'Calendar') && (contentType !== 'Workflows') ? new ContentCollection({ viewType: 'thumbnails', page: 1, count: 20, parrentContentId: parrentContentId }) : new ContentCollection();
+                collection = (contentType !== 'Calendar') && (contentType !== 'Workflows') ? new ContentCollection({ viewType: 'thumbnails', page: 1, count: 4, parrentContentId: parrentContentId }) : new ContentCollection();
                 collection.bind('reset', _.bind(createViews, self));
                 function createViews() {
                     var contentView = new ContentView({ collection: collection });
