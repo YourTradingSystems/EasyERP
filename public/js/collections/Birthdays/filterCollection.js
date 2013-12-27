5cd0a787f0b09540b4ac31538d390dcad24803ff
romashka50 <roma.buchuk@gmail.com>
2013-12-27 19:44:45 +0200

    Birthdays

---
+++ public/js/collections/Birthdays/filterCollection.js
@@ -0,0 +1,35 @@
+﻿define([
+    'models/EmployeesModel',
+    'common'
+],
+    function (EmployeeModel, common) {
+        var EmployeesCollection = Backbone.Collection.extend({
+            model: EmployeeModel,
+            url: "/Birthdays",
+            initialize: function (options) {
+                this.fetch({
+                    reset: true,
+                    success: function() {
+                        console.log("Birthdays fetchSuccess");
+                    },
+                    error: this.fetchError
+                });
+            },
+
+            parse: true,
+            parse: function (response) {
+                if (response.data) {
+                    _.map(response.data, function (employee) {
+                        employee.createdBy.date = common.utcDateToLocaleDateTime(employee.createdBy.date);
+                        employee.editedBy.date = common.utcDateToLocaleDateTime(employee.editedBy.date);
+                        return employee;
+                    });
+                }
+                return response.data;
+            }
+
+            
+        });
+
+        return EmployeesCollection;
+    });
