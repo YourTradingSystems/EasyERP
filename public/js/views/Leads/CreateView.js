85405cab49869e017561adb18ab8818c3f0f6d4a
Ivan Kornyk <ownmass@gmail.com>
2013-12-31 13:58:17 +0200

    leads grops

--- public/js/views/Leads/CreateView.js
+++ public/js/views/Leads/CreateView.js
@@ -19,9 +19,148 @@ define([
             events: {
                 "click #tabList a": "switchTab",
                 "change #customer": "selectCustomer",
-                "change #workflowNames": "changeWorkflows"
-            },
+                "change #workflowNames": "changeWorkflows",
 
+                'keydown': 'keydownHandler',
+                'click .dialog-tabs a': 'changeTab',
+                'click .addUser': 'addUser',
+                'click .addGroup': 'addGroup',
+                'click .unassign': 'unassign',
+                "click .prevUserList":"prevUserList",
+                "click .nextUserList":"nextUserList"
+            },
+            keydownHandler: function(e){
+                switch (e.which){
+                    case 27:
+                        this.hideDialog();
+                        break;
+                    default:
+                        break;
+                }
+            },
+            changeTab:function(e){
+                $(e.target).closest(".dialog-tabs").find("a.active").removeClass("active");
+                $(e.target).addClass("active");
+                var n = $(e.target).parents(".dialog-tabs").find("li").index($(e.target).parent());
+                $(".dialog-tabs-items").find(".dialog-tabs-item.active").removeClass("active");
+                $(".dialog-tabs-items").find(".dialog-tabs-item").eq(n).addClass("active");
+            },
+            addUser:function(e){
+                var self = this;
+                $(".addUserDialog").dialog({
+                    dialogClass: "add-user-dialog",
+                    width: "900px",
+                    buttons:{
+                        save:{
+                            text:"Choose",
+                            class:"btn",
+
+                            click: function(){
+                                click: self.addUserToTable("#targetUsers")
+                                $( this ).dialog( "close" );
+                            }
+
+                        },
+                        cancel:{
+                            text:"Cancel",
+                            class:"btn",
+                            click: function(){
+                                $( this ).dialog( "close" );
+                            }
+                        }
+                    }
+
+                });
+                $("#targetUsers").unbind().on("click","li",this.removeUsers);
+                $("#sourceUsers").unbind().on("click","li",this.addUsers);
+                var self = this;
+                $(".nextUserList").unbind().on("click",function(e){
+                    self.page+=1
+                    self.nextUserList(e,self.page)
+                });
+                $(".prevUserList").unbind().on("click",function(e){
+                    self.page-=1
+                    self.prevUserList(e,self.page)
+                });
+            },
+            addUserToTable:function(id){
+                $(".groupsAndUser").show();
+                $(".groupsAndUser tr").each(function(){
+                    if ($(this).data("type")==id.replace("#","")){
+                        $(this).remove();
+                    }
+                });
+                $(id).find("li").each(function(){
+                    $(".groupsAndUser").append("<tr data-type='"+id.replace("#","")+"' data-id='"+ $(this).attr("id")+"'><td>"+$(this).text()+"</td><td class='text-right'></td></tr>");
+                });
+                if ($(".groupsAndUser tr").length<2){
+                    $(".groupsAndUser").hide();
+                }
+            },
+            addGroup:function(e){
+                var self = this;
+                $(".addGroupDialog").dialog({
+                    dialogClass: "add-group-dialog",
+                    width: "900px",
+                    buttons:{
+                        save:{
+                            text:"Choose",
+                            class:"btn",
+                            click: function(){
+                                self.addUserToTable("#targetGroups")
+                                $( this ).dialog( "close" );
+                            }
+                        },
+                        cancel:{
+                            text:"Cancel",
+                            class:"btn",
+                            click: function(){
+                                $( this ).dialog( "close" );
+                            }
+                        }
+                    }
+
+                });
+                $("#targetGroups").unbind().on("click","li",this.removeUsers);
+                $("#sourceGroups").unbind().on("click","li",this.addUsers);
+                var self = this;
+                $(".nextGroupList").unbind().on("click",function(e){
+                    self.pageG+=1
+                    self.nextUserList(e,self.pageG)
+                });
+                $(".prevGroupList").unbind().on("click",function(e){
+                    self.pageG-=1
+                    self.prevUserList(e,self.pageG)
+                });
+
+            },
+            unassign:function(e){
+                var id=$(e.target).closest("tr").data("id");
+                var type=$(e.target).closest("tr").data("type");
+                var text=$(e.target).closest("tr").find("td").eq(0).text();
+                $("#"+type).append("<option value='"+id+"'>"+text+"</option>");
+                $(e.target).closest("tr").remove();
+                if ($(".groupsAndUser").find("tr").length==1){
+                    $(".groupsAndUser").hide();
+                }
+
+            },
+            nextUserList:function(e,page){
+                common.populateUsersForGroups('#sourceUsers','#targetUsers',null,page);
+            },
+            prevUserList:function(e,page){
+                common.populateUsersForGroups('#sourceUsers','#targetUsers',null,page);
+            },
+            addUsers: function (e) {
+                e.preventDefault();
+                $(e.target).closest(".ui-dialog").find(".target").append($(e.target));
+
+            },
+            removeUsers: function (e) {
+                e.preventDefault();
+                $(e.target).closest(".ui-dialog").find(".source").append($(e.target));
+            },
+//========================
             changeWorkflows: function () {
                 var name = this.$("#workflowNames option:selected").val();
                 var value = this.workflowsCollection.findWhere({ name: name }).toJSON().value;
@@ -80,6 +219,18 @@ define([
                 var optout = (this.$el.find("#optout").is(":checked")) ? true : false;
                 var reffered = $.trim(this.$el.find("#reffered").val());
 
+                var usersId=[];
+                var groupsId=[];
+                $(".groupsAndUser tr").each(function(){
+                    if ($(this).data("type")=="targetUsers"){
+                        usersId.push($(this).data("id"));
+                    }
+                    if ($(this).data("type")=="targetGroups"){
+                        groupsId.push($(this).data("id"));
+                    }
+
+                });
+                var whoCanRW = this.$el.find("[name='whoCanRW']:checked").val();
                 this.model.save({
                     name: name,
                     company: company,
@@ -99,7 +250,13 @@ define([
                     active: active,
                     optout: optout,
                     reffered: reffered,
-                    workflow: workflow
+                    workflow: workflow,
+                    groups: {
+                        owner: $("#allUsers").val(),
+                        users: usersId,
+                        group: groupsId
+                    },
+                    whoCanRW: whoCanRW
                 },
                     {
                         headers: {
@@ -144,7 +301,11 @@ define([
 						}]
 
                 });
-				common.populateCustomers(App.ID.customerDd, "/Customer");
+                common.populateUsersForGroups('#sourceUsers','#targetUsers',null,this.page);
+                common.populateUsers("#allUsers", "/Users",null,null,true);
+                common.populateDepartmentsList("#sourceGroups","#targetGroups", "/Departments",null,this.pageG);
+
+                common.populateCustomers(App.ID.customerDd, "/Customer");
                 common.populateDepartments(App.ID.salesTeam, "/Departments");
                 common.populateEmployeesDd(App.ID.salesPerson, "/getPersonsForDd");
                 common.populatePriority(App.ID.priorityDd, "/Priority");
