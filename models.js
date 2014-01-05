5e82856528e2989ac7ad1ae1a80e6c2a2d830e22
romashka50 <roma.buchuk@gmail.com>
2014-01-05 19:56:36 +0200

    serverFixes

---
+++ models.js
@@ -0,0 +1,9 @@
+﻿var models = function (dbsArray) {
+    function get(id, collection, schema) {
+        return dbsArray[id].model(collection, schema);
+    }
+    return {
+        get: get
+    }
+};
+module.exports = models;
