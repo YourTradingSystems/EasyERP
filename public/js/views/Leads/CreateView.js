da3ff16ee9539937901a91359f88832cc05b138c
Andrew <andrew.yanuta@gmail.com>
2014-02-17 16:42:02 +0200

    kanban edit button login template Leads email validation fixed

--- public/js/views/Leads/CreateView.js
+++ public/js/views/Leads/CreateView.js
@@ -2,7 +2,7 @@ define([
     "text!templates/Leads/CreateTemplate.html",
     "models/LeadsModel",
     "common",
-	"populate"
+    "populate"
 ],
     function (CreateTemplate, LeadModel, common, populate) {
 
@@ -14,9 +14,9 @@ define([
             initialize: function () {
                 _.bindAll(this, "saveItem", "render");
                 this.model = new LeadModel();
-                this.page=1;
-                this.pageG=1;
-				this.responseObj = {};
+                this.page = 1;
+                this.pageG = 1;
+                this.responseObj = {};
                 this.render();
             },
 
@@ -29,8 +29,8 @@ define([
                 'click .addUser': 'addUser',
                 'click .addGroup': 'addGroup',
                 'click .unassign': 'unassign',
-                "click .prevUserList":"prevUserList",
-                "click .nextUserList":"nextUserList",
+                "click .prevUserList": "prevUserList",
+                "click .nextUserList": "nextUserList",
                 "click .newSelectList li:not(.miniStylePagination)": "chooseOption",
                 "click .newSelectList li.miniStylePagination": "notHide",
                 "click .newSelectList li.miniStylePagination .next:not(.disabled)": "nextSelect",
@@ -38,25 +38,25 @@ define([
                 "click": "hideNewSelect",
                 "click .current-selected": "showNewSelect"
             },
-			notHide: function () {
-				return false;
-            },
-			nextSelect:function(e){
-				this.showNewSelect(e,false,true);
-			},
-			prevSelect:function(e){
-				this.showNewSelect(e,true,false);
-			},
-            showNewSelect:function(e,prev,next){
-                populate.showSelect(e,prev,next,this);
+            notHide: function () {
                 return false;
             },
-			chooseOption:function(e){
-                $(e.target).parents("dd").find(".current-selected").text($(e.target).text()).attr("data-id",$(e.target).attr("id"));
-			},
+            nextSelect: function (e) {
+                this.showNewSelect(e, false, true);
+            },
+            prevSelect: function (e) {
+                this.showNewSelect(e, true, false);
+            },
+            showNewSelect: function (e, prev, next) {
+                populate.showSelect(e, prev, next, this);
+                return false;
+            },
+            chooseOption: function (e) {
+                $(e.target).parents("dd").find(".current-selected").text($(e.target).text()).attr("data-id", $(e.target).attr("id"));
+            },
 
-            keydownHandler: function(e){
-                switch (e.which){
+            keydownHandler: function (e) {
+                switch (e.which) {
                     case 27:
                         this.hideDialog();
                         break;
@@ -65,7 +65,7 @@ define([
                 }
             },
 
-            changeTab:function(e){
+            changeTab: function (e) {
                 var holder = $(e.target);
                 holder.closest(".dialog-tabs").find("a.active").removeClass("active");
                 holder.addClass("active");
@@ -75,117 +75,117 @@ define([
                 dialog_holder.find(".dialog-tabs-item").eq(n).addClass("active");
             },
 
-            addUser:function(){
+            addUser: function () {
                 var self = this;
                 $(".addUserDialog").dialog({
                     dialogClass: "add-user-dialog",
                     width: "900px",
-                    buttons:{
-                        save:{
-                            text:"Choose",
-                            class:"btn",
+                    buttons: {
+                        save: {
+                            text: "Choose",
+                            class: "btn",
 
-                            click: function(){
+                            click: function () {
                                 self.addUserToTable("#targetUsers");
-                                $( this ).dialog( "close" );
+                                $(this).dialog("close");
                             }
 
                         },
-                        cancel:{
-                            text:"Cancel",
-                            class:"btn",
-                            click: function(){
-                                $( this ).dialog( "close" );
+                        cancel: {
+                            text: "Cancel",
+                            class: "btn",
+                            click: function () {
+                                $(this).dialog("close");
                             }
                         }
                     }
 
                 });
-                $("#targetUsers").unbind().on("click","li",this.removeUsers);
-                $("#sourceUsers").unbind().on("click","li",this.addUsers);
-                $(".nextUserList").unbind().on("click",function(e){
+                $("#targetUsers").unbind().on("click", "li", this.removeUsers);
+                $("#sourceUsers").unbind().on("click", "li", this.addUsers);
+                $(".nextUserList").unbind().on("click", function (e) {
                     self.page += 1;
-                    self.nextUserList(e,self.page);
+                    self.nextUserList(e, self.page);
                 });
-                $(".prevUserList").unbind().on("click",function(e){
+                $(".prevUserList").unbind().on("click", function (e) {
                     self.page -= 1;
-                    self.prevUserList(e,self.page);
+                    self.prevUserList(e, self.page);
                 });
             },
 
-            addUserToTable:function(id){
+            addUserToTable: function (id) {
                 var groupsAndUser_holder = $(".groupsAndUser");
                 var groupsAndUserHr_holder = $(".groupsAndUser tr");
                 groupsAndUser_holder.show();
-                groupsAndUserHr_holder.each(function(){
-                    if ($(this).data("type")==id.replace("#","")){
+                groupsAndUserHr_holder.each(function () {
+                    if ($(this).data("type") == id.replace("#", "")) {
                         $(this).remove();
                     }
                 });
-                $(id).find("li").each(function(){
-                    groupsAndUser_holder.append("<tr data-type='"+id.replace("#","")+"' data-id='"+ $(this).attr("id")+"'><td>"+$(this).text()+"</td><td class='text-right'></td></tr>");
+                $(id).find("li").each(function () {
+                    groupsAndUser_holder.append("<tr data-type='" + id.replace("#", "") + "' data-id='" + $(this).attr("id") + "'><td>" + $(this).text() + "</td><td class='text-right'></td></tr>");
                 });
-                if (groupsAndUserHr_holder.length<2){
+                if (groupsAndUserHr_holder.length < 2) {
                     groupsAndUser_holder.hide();
                 }
             },
 
-            addGroup:function(){
+            addGroup: function () {
                 var self = this;
                 $(".addGroupDialog").dialog({
                     dialogClass: "add-group-dialog",
                     width: "900px",
-                    buttons:{
-                        save:{
-                            text:"Choose",
-                            class:"btn",
-                            click: function(){
+                    buttons: {
+                        save: {
+                            text: "Choose",
+                            class: "btn",
+                            click: function () {
                                 self.addUserToTable("#targetGroups");
-                                $( this ).dialog( "close" );
+                                $(this).dialog("close");
                             }
                         },
-                        cancel:{
-                            text:"Cancel",
-                            class:"btn",
-                            click: function(){
-                                $( this ).dialog( "close" );
+                        cancel: {
+                            text: "Cancel",
+                            class: "btn",
+                            click: function () {
+                                $(this).dialog("close");
                             }
                         }
                     }
 
                 });
-                $("#targetGroups").unbind().on("click","li",this.removeUsers);
-                $("#sourceGroups").unbind().on("click","li",this.addUsers);
-                $(".nextGroupList").unbind().on("click",function(e){
+                $("#targetGroups").unbind().on("click", "li", this.removeUsers);
+                $("#sourceGroups").unbind().on("click", "li", this.addUsers);
+                $(".nextGroupList").unbind().on("click", function (e) {
                     self.pageG += 1;
-                    self.nextUserList(e,self.pageG);
+                    self.nextUserList(e, self.pageG);
                 });
-                $(".prevGroupList").unbind().on("click",function(e){
+                $(".prevGroupList").unbind().on("click", function (e) {
                     self.pageG -= 1;
-                    self.prevUserList(e,self.pageG);
+                    self.prevUserList(e, self.pageG);
                 });
 
             },
 
-            unassign:function(e){
+            unassign: function (e) {
                 var holder = $(e.target);
                 var id = holder.closest("tr").data("id");
                 var type = holder.closest("tr").data("type");
                 var text = holder.closest("tr").find("td").eq(0).text();
-                $("#"+type).append("<option value='"+id+"'>"+text+"</option>");
+                $("#" + type).append("<option value='" + id + "'>" + text + "</option>");
                 holder.closest("tr").remove();
                 var groupsAndUser_holder = $(".groupsAndUser");
-                if (groupsAndUser_holder.find("tr").length==1){
+                if (groupsAndUser_holder.find("tr").length == 1) {
                     groupsAndUser_holder.hide();
                 }
             },
 
-            nextUserList:function(e,page){
-                common.populateUsersForGroups('#sourceUsers','#targetUsers',null,page);
+            nextUserList: function (e, page) {
+                common.populateUsersForGroups('#sourceUsers', '#targetUsers', null, page);
             },
 
-            prevUserList:function(e,page){
-                common.populateUsersForGroups('#sourceUsers','#targetUsers',null,page);
+            prevUserList: function (e, page) {
+                common.populateUsersForGroups('#sourceUsers', '#targetUsers', null, page);
             },
 
             addUsers: function (e) {
@@ -216,7 +216,7 @@ define([
             },
 
             saveItem: function () {
-				var self = this;
+                var self = this;
                 var $company = this.$("#company");
                 var mid = 39;
                 var name = $.trim(this.$el.find("#name").val());
@@ -239,7 +239,7 @@ define([
                     first: first,
                     last: last
                 };
-                var email = $.trim(this.$el.find("#email").val());
+                var email = $.trim(this.$el.find("#e-mail").val());
                 var func = $.trim(this.$el.find("#func").val());
 
                 var phone = $.trim(this.$el.find("#phone").val());
@@ -257,52 +257,52 @@ define([
                 var optout = (this.$el.find("#optout").is(":checked")) ? true : false;
                 var reffered = $.trim(this.$el.find("#reffered").val());
 
-                var usersId=[];
-                var groupsId=[];
-                $(".groupsAndUser tr").each(function(){
-                    if ($(this).data("type")=="targetUsers"){
+                var usersId = [];
+                var groupsId = [];
+                $(".groupsAndUser tr").each(function () {
+                    if ($(this).data("type") == "targetUsers") {
                         usersId.push($(this).data("id"));
                     }
-                    if ($(this).data("type")=="targetGroups"){
+                    if ($(this).data("type") == "targetGroups") {
                         groupsId.push($(this).data("id"));
                     }
 
                 });
                 var whoCanRW = this.$el.find("[name='whoCanRW']:checked").val();
                 this.model.save({
-                    name: name,
-                    company: company,
-                    campaign: $('#campaignDd').data("id"),
-                    source: $('#sourceDd').data("id"),
-                    customer: idCustomer,
-                    address: address,
-                    salesPerson: salesPersonId,
-                    salesTeam: salesTeamId,
-                    contactName: contactName,
-                    email: email,
-                    func: func,
-                    phones: phones,
-                    fax: fax,
-                    priority: priority,
-                    internalNotes: internalNotes,
-                    active: active,
-                    optout: optout,
-                    reffered: reffered,
-                    workflow: workflow,
-                    groups: {
-                        owner: $("#allUsers").val(),
-                        users: usersId,
-                        group: groupsId
+                        name: name,
+                        company: company,
+                        campaign: $('#campaignDd').data("id"),
+                        source: $('#sourceDd').data("id"),
+                        customer: idCustomer,
+                        address: address,
+                        salesPerson: salesPersonId,
+                        salesTeam: salesTeamId,
+                        contactName: contactName,
+                        email: email,
+                        func: func,
+                        phones: phones,
+                        fax: fax,
+                        priority: priority,
+                        internalNotes: internalNotes,
+                        active: active,
+                        optout: optout,
+                        reffered: reffered,
+                        workflow: workflow,
+                        groups: {
+                            owner: $("#allUsers").val(),
+                            users: usersId,
+                            group: groupsId
+                        },
+                        whoCanRW: whoCanRW
                     },
-                    whoCanRW: whoCanRW
-                },
                     {
                         headers: {
                             mid: mid
                         },
-                        
+
                         success: function () {
-							self.hideDialog();
+                            self.hideDialog();
                             Backbone.history.navigate("easyErp/Leads", { trigger: true });
                         },
                         error: function (model, xhr) {
@@ -325,37 +325,42 @@ define([
             },
 
             render: function () {
-				var self=this;
+                var self = this;
                 var formString = this.template();
                 this.$el = $(formString).dialog({
-					closeOnEscape: false,
-                    autoOpen:true,
-                    resizable:true,
-					dialogClass:"edit-dialog",
-					title: "Edit Company",
-					width:"800",
+                    closeOnEscape: false,
+                    autoOpen: true,
+                    resizable: true,
+                    dialogClass: "edit-dialog",
+                    title: "Edit Company",
+                    width: "800",
                     buttons: [
                         {
                             text: "Create",
-                            click: function () { self.saveItem(); }
+                            click: function () {
+                                self.saveItem();
+                            }
                         },
 
-						{
-							text: "Cancel",
-							click: function () { self.hideDialog(); }
-						}]
+                        {
+                            text: "Cancel",
+                            click: function () {
+                                self.hideDialog();
+                            }
+                        }
+                    ]
 
                 });
-                common.populateUsersForGroups('#sourceUsers','#targetUsers',null,this.page);
-                common.populateUsers("#allUsers", "/UsersForDd",null,null,true);
-                common.populateDepartmentsList("#sourceGroups","#targetGroups", "/DepartmentsForDd",null,this.pageG);
+                common.populateUsersForGroups('#sourceUsers', '#targetUsers', null, this.page);
+                common.populateUsers("#allUsers", "/UsersForDd", null, null, true);
+                common.populateDepartmentsList("#sourceGroups", "#targetGroups", "/DepartmentsForDd", null, this.pageG);
 
-				populate.getPriority("#priorityDd",this,true);
-				populate.getWorkflow("#workflowsDd","#workflowNamesDd","/WorkflowsForDd",{id:"Leads"},"name",this,true);			
-				populate.get2name("#customerDd", "/Customer",{},this,true,true);	
-				populate.get("#sourceDd", "/sources",{},"name",this,true,true);	
-				populate.get("#campaignDd", "/Campaigns",{},"name",this,true,true);	
-				populate.get2name("#salesPerson", "/getForDdByRelatedUser",{},this,true,true);	
+                populate.getPriority("#priorityDd", this, true);
+                populate.getWorkflow("#workflowsDd", "#workflowNamesDd", "/WorkflowsForDd", {id: "Leads"}, "name", this, true);
+                populate.get2name("#customerDd", "/Customer", {}, this, true, true);
+                populate.get("#sourceDd", "/sources", {}, "name", this, true, true);
+                populate.get("#campaignDd", "/Campaigns", {}, "name", this, true, true);
+                populate.get2name("#salesPerson", "/getForDdByRelatedUser", {}, this, true, true);
                 this.delegateEvents(this.events);
                 return this;
             }
