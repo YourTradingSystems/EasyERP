4d29ef1efc250ce95f521bd5a71b51c7b6baff95
Burdun <burdunwasya@mail.ru>
2014-03-06 16:28:40 +0200

    Edit Workflow

--- Modules/Workflow.js
+++ Modules/Workflow.js
@@ -112,19 +112,29 @@ var Workflow = function (logWriter, mongoose, models) {
             
             try {
                 if (data) {
-                    delete data._id;
-                    models.get(req.session.lastDb - 1, "workflows", workflowSchema).update({ _id: _id }, data, function (err, res) {
-                        if (err) {
-                            console.log(err);
-                            logWriter.log('WorkFlow.js update workflow.update ' + err);
-                            result.send(400, { error: 'WorkFlow.js update workflow error ' });
-                            return;
-                        } else {
-                            
-                            result.send(200, { success: 'WorkFlow update success' });
-                        }
+                 models.get(req.session.lastDb - 1, "workflows", workflowSchema).find({ _id: _id }, function (err, workflows) {//add vasya no not create workflow with the same name
+                     models.get(req.session.lastDb - 1, "workflows", workflowSchema).find({ $and: [{ wId: workflows[0].wId  },{name: data.name}]}, function (err, workflow) {//add vasya no not create workflow with the same name
+                            delete data._id;
+                            if (workflow.length > 0) {//add vasya no not create workflow with the same name
+                                     if (workflow[0].name === data.name) {//add vasya no not create workflow with the same name
+                                            result.send(400, { error: 'An Workflows with the same Name already exists' });//add vasya no not create workflow with the same name
+                                     }
+                               }
+                            else if(workflow.length === 0) {//add vasya no not create workflow with the same name
+                                models.get(req.session.lastDb - 1, "workflows", workflowSchema).update({ _id: _id }, data, function (err, res) {
+                                    if (err) {
+                                        console.log(err);
+                                        logWriter.log('WorkFlow.js update workflow.update ' + err);
+                                        result.send(400, { error: 'WorkFlow.js update workflow error ' });
+                                        return;
+                                    } else {
+                                        result.send(200, { success: 'WorkFlow update success' });
+                                    }
+                                });
+                            }
+                       });
                     });
-                }
+                    }
             }
             catch (exception) {
                 logWriter.log("Workflow.js  create " + exception);
