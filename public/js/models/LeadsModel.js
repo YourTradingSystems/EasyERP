28d9d38ece6d7a9feafef927fd6b0b6f833a9f19
VasyaBurdun <burdunwasya@mail.ru>
2014-03-20 15:54:34 +0200

    fix bug validation zip date of birth, cursor attach

--- public/js/models/LeadsModel.js
+++ public/js/models/LeadsModel.js
@@ -16,7 +16,7 @@
             Validation.checkGroupsNameField(errors, true, attrs.name, "Subject");
             Validation.checkNameField(errors, false, attrs.contactName.first, "Contact first name");
             Validation.checkNameField(errors, false, attrs.contactName.last, "Contact last name");
-            //Validation.checkNameField(errors, false, attrs.company.name, "Company");
+            Validation.checkGroupsNameField(errors, false, attrs.company.name, "Company");
             Validation.checkPhoneField(errors, false, attrs.phones.phone, "Phone");
             Validation.checkPhoneField(errors, false, attrs.phones.mobile, "Mobile");
             Validation.checkCountryCityStateField(errors, false, attrs.address.country, "Country");
